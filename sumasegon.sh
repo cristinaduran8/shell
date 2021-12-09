999912a8488d3012eaf5800c13cfe0bccb0d29a6
cristinaduran8 <93319981+cristinaduran8@users.noreply.github.com>
2021-12-09 11:54:53 +0100

    Create sumasegon.sh

---
+++ sumasegon.sh
@@ -0,0 +1,8 @@
+ #exercicisumadeunsegon
+  #Cristina
+
+echo "Dim un numero que li sumare un segon"
+read num
+echo hores=$((num/3600))hores
+echo $[((%3600))/60]minuts
+echo $[((num%3600))%60]segons
